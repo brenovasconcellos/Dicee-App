80fcb261addad0bff3158a65700558129cfd0139
Breno Vasconcellos <dv.brenovasconcellos@gmail.com>
2021-12-15 22:41:13 -0300

    first commit

---
+++ Dicee-App/Dicee_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_AppApp.swift
+//  Dicee-App
+//
+//  Created by Breno Vasconcellos on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
